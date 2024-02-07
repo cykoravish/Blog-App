9ab9f22c48b4babc9c8ca3a800869b9331287663
Ravish Bisht <ravishbisht85@gmail.com>
2024-02-07 09:39:58 +0530

    signup route add

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -11,8 +12,11 @@ mongoose
   .catch((err) => console.log("error in db connection. error: ", err));
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running at port 3000");
 });
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoutes);
