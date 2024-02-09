bbc8e7b20e6cc5a460bf48f285728fd9a55c4fdc
Ravish Bisht <ravishbisht85@gmail.com>
2024-02-09 10:07:42 +0530

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -41,7 +41,7 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "invalid password"));
     }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const {password: pass, ...rest} = validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
     return res
       .status(200)
       .cookie("access_token", token, { httpOnly: true })
